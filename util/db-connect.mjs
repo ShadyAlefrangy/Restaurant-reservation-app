86f4ada9500f7c75598686dae57a350b44fab8e8
Shady Alefrangy <shadyahmed38@gmail.com>
2022-08-07 21:45:47 +0300

    Update DB Password

--- util/db-connect.mjs
+++ util/db-connect.mjs
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function dbConnect() {
     try {
-        await mongoose.connect('mongodb+srv://shady:shady329921@cluster0.4rv4jja.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://shady:*******@cluster0.4rv4jja.mongodb.net/?retryWrites=true&w=majority');
         console.log('Connected to MongoDB');
         return mongoose.connection;
     } catch (error) {
